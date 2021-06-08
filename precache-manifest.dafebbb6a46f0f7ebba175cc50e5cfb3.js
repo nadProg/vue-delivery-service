3b52132986375d1f4aa31500c32d887d84ade0d1
Eugene Skrobov <skrobov.ev@gmail.com>
2021-06-08 19:08:30 +0700

    deploy

---
+++ precache-manifest.dafebbb6a46f0f7ebba175cc50e5cfb3.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfd234108f0368430c6d20f9dd22655",
+    "url": "/vue-delivery-service/android-chrome-192x192.png"
+  },
+  {
+    "revision": "1ec8b4004307cf9abc5cfd56045b1139",
+    "url": "/vue-delivery-service/android-chrome-512x512.png"
+  },
+  {
+    "revision": "317fde8f11cbcfcd0770d60a47101151",
+    "url": "/vue-delivery-service/apple-touch-icon.png"
+  },
+  {
+    "revision": "b0df1d8364886483f481bc261ea8db4b",
+    "url": "/vue-delivery-service/browserconfig.xml"
+  },
+  {
+    "revision": "3362fbd62a721d47aa69",
+    "url": "/vue-delivery-service/css/app.d576861b.css"
+  },
+  {
+    "revision": "72dd3c90f13ce98840dc",
+    "url": "/vue-delivery-service/css/chunk-e9284438.b6450a52.css"
+  },
+  {
+    "revision": "d722f002e315b5ee3011",
+    "url": "/vue-delivery-service/css/chunk-eb63ef72.6873a936.css"
+  },
+  {
+    "revision": "2b4e55d55dcd0c3c1290",
+    "url": "/vue-delivery-service/css/chunk-f735233a.f3478954.css"
+  },
+  {
+    "revision": "c3f862568b043327a165",
+    "url": "/vue-delivery-service/css/chunk-vendors.f32eaac2.css"
+  },
+  {
+    "revision": "f4d26bdb325228442db45f4e19244c84",
+    "url": "/vue-delivery-service/favicon-16x16.png"
+  },
+  {
+    "revision": "e6af8e91730e4d00d9940287f64b8d78",
+    "url": "/vue-delivery-service/favicon-32x32.png"
+  },
+  {
+    "revision": "750704d947243e692aca87658d366c8c",
+    "url": "/vue-delivery-service/fonts/Montserrat-Bold.750704d9.woff2"
+  },
+  {
+    "revision": "b55e6e808057b12beea40fdced35277c",
+    "url": "/vue-delivery-service/fonts/Montserrat-Bold.b55e6e80.woff"
+  },
+  {
+    "revision": "6b72a2966907573bfb60e5736e409db2",
+    "url": "/vue-delivery-service/fonts/Montserrat-Medium.6b72a296.woff2"
+  },
+  {
+    "revision": "9f2ae36a53edcac31a212dbd56eb747b",
+    "url": "/vue-delivery-service/fonts/Montserrat-Medium.9f2ae36a.woff"
+  },
+  {
+    "revision": "dd821ebb8b27ac6a057fcca1d8f174cf",
+    "url": "/vue-delivery-service/fonts/Montserrat-Regular.dd821ebb.woff2"
+  },
+  {
+    "revision": "e5d5b4891966f1ad515c8758a5c09350",
+    "url": "/vue-delivery-service/fonts/Montserrat-Regular.e5d5b489.woff"
+  },
+  {
+    "revision": "2dcc29341cfdae41c7e21781c6e4203c",
+    "url": "/vue-delivery-service/fonts/Montserrat-SemiBold.2dcc2934.woff2"
+  },
+  {
+    "revision": "98bf0fc7667a81a7054b483a98e83ee4",
+    "url": "/vue-delivery-service/fonts/Montserrat-SemiBold.98bf0fc7.woff"
+  },
+  {
+    "revision": "4b62b4dfb747419d7d52301e3ae62bec",
+    "url": "/vue-delivery-service/img/icon-location.4b62b4df.svg"
+  },
+  {
+    "revision": "137c7553f905fe97e56648d4859b82d0",
+    "url": "/vue-delivery-service/img/link-checked.137c7553.jpg"
+  },
+  {
+    "revision": "de8e3e9494653156d5fcd2b18e285510",
+    "url": "/vue-delivery-service/img/map.de8e3e94.jpg"
+  },
+  {
+    "revision": "9f6168e88f90baf50d6615648277ca19",
+    "url": "/vue-delivery-service/index.html"
+  },
+  {
+    "revision": "3362fbd62a721d47aa69",
+    "url": "/vue-delivery-service/js/app.9dc065f0.js"
+  },
+  {
+    "revision": "356c34d1aaa87f69531b",
+    "url": "/vue-delivery-service/js/chunk-2d0c1b37.94fc1db0.js"
+  },
+  {
+    "revision": "ad01783bac00229dc5a1",
+    "url": "/vue-delivery-service/js/chunk-2d0e59b0.42fd7640.js"
+  },
+  {
+    "revision": "adbbf37d266e142cee90",
+    "url": "/vue-delivery-service/js/chunk-2d216642.1fa1ba91.js"
+  },
+  {
+    "revision": "366724b0079180e9c8b7",
+    "url": "/vue-delivery-service/js/chunk-3629a5e2.e07fdd6a.js"
+  },
+  {
+    "revision": "f8702b59e7310458cc72",
+    "url": "/vue-delivery-service/js/chunk-419f8b2e.0584aac3.js"
+  },
+  {
+    "revision": "72dd3c90f13ce98840dc",
+    "url": "/vue-delivery-service/js/chunk-e9284438.f319f8cb.js"
+  },
+  {
+    "revision": "d722f002e315b5ee3011",
+    "url": "/vue-delivery-service/js/chunk-eb63ef72.78f2cd66.js"
+  },
+  {
+    "revision": "2b4e55d55dcd0c3c1290",
+    "url": "/vue-delivery-service/js/chunk-f735233a.e2eaa896.js"
+  },
+  {
+    "revision": "c3f862568b043327a165",
+    "url": "/vue-delivery-service/js/chunk-vendors.afbcb0a9.js"
+  },
+  {
+    "revision": "30322945cac3f5c876c338548d55629d",
+    "url": "/vue-delivery-service/manifest.json"
+  },
+  {
+    "revision": "593b27bcf436973b8ffe89cb0f38a90e",
+    "url": "/vue-delivery-service/mstile-144x144.png"
+  },
+  {
+    "revision": "3d3e9b9acb45628768ef8b9e3f428932",
+    "url": "/vue-delivery-service/mstile-150x150.png"
+  },
+  {
+    "revision": "e63aeacca99a905ea4ee21c126ec1fed",
+    "url": "/vue-delivery-service/mstile-310x150.png"
+  },
+  {
+    "revision": "c65e90c42b86699156e16f85a397cf39",
+    "url": "/vue-delivery-service/mstile-310x310.png"
+  },
+  {
+    "revision": "e920b8a430c0548f1b3202faf84b23ed",
+    "url": "/vue-delivery-service/mstile-70x70.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-delivery-service/robots.txt"
+  },
+  {
+    "revision": "9653c87ca54bad8d8f9043644b7978a4",
+    "url": "/vue-delivery-service/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "/vue-delivery-service/site.webmanifest"
+  }
+]);
